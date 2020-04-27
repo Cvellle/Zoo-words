f71934d657ea28f1f85f3ee6016a80031606b932
ncvetic <cuekisa@yahoo.com>
2020-04-27 23:24:22 +0200

    Updates

---
+++ precache-manifest.097fe48d983a0c4765dbcac4bf058586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcfb8e8c2fe2769bb7ff5cf9fb32593",
+    "url": "/Zoo-words/index.html"
+  },
+  {
+    "revision": "a2a3fc4564da8baed849",
+    "url": "/Zoo-words/static/css/main.6c96d6ac.chunk.css"
+  },
+  {
+    "revision": "537513fa008112a7fc2e",
+    "url": "/Zoo-words/static/js/2.5676fd87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Zoo-words/static/js/2.5676fd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a3fc4564da8baed849",
+    "url": "/Zoo-words/static/js/main.3824ca75.chunk.js"
+  },
+  {
+    "revision": "2e3e23661db9fcf1077d",
+    "url": "/Zoo-words/static/js/runtime-main.ba8297fa.js"
+  }
+]);
